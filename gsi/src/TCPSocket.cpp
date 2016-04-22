d12fbcd8ce175200d282457c61457d9e85a5785c
JosephFoster118 <Joseph.Foster118@gmail.com>
2016-04-22 18:58:52 -0500

    Added TCPSocket
    Added TCPSocket

---
+++ gsi/src/TCPSocket.cpp
@@ -0,0 +1,49 @@
+#include <gsi/TCPSocket.h>
+
+// @TODO: create a network class to hold byte swap info
+
+bool do_byte_swap = (1 != ntohs(1));
+
+float hostToNetFloat(float val)
+{
+	return netToHostFloat(val);
+}
+
+float netToHostFloat(float val)
+{
+	if (do_byte_swap)
+	{
+		uint32_t in_val;
+		float *fp = (float *)(&in_val);
+		*fp = val;
+		in_val = htonl(in_val);
+		return (*fp);
+	}
+	else
+	{
+		return val;
+	}
+}
+
+namespace gsi
+{
+	
+	TCPSocket::TCPSocket()
+	{
+#if defined (PTHREADS)
+		printf("Creating Sovket\n");
+#endif
+		
+	}
+	
+	TCPSocket::TCPSocket(uint16_t port, SocketType st)
+	{
+#if defined (PTHREADS)
+		printf("Creating Sovket\n");
+#endif
+	}
+	
+	
+}
+
+
